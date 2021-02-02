c6e5e73f5f7ca4724f15aefed13ca6fc9325503b
andrejlevkovitch <andrejlevkovitch@gmail.com>
2021-02-02 16:15:33 +0300

    change tests to bdd-style

--- test/remap.cu
+++ test/remap.cu
@@ -9,18 +9,22 @@
 #include "cuda/remap.hpp"
 #include <random>
 
-TEST_CASE("remap", "[remap]") {
-  SECTION("try remap empty matrix") {
+SCENARIO("remap", "[remap]") {
+  GIVEN("empty matrix") {
     cuda::gpu_mat<int>  input{0, 0, 0};
     cuda::gpu_mat<int>  output{0, 0};
     cuda::gpu_mat<int2> map{0, 0};
 
-    cuda::remap(input, output, map);
+    WHEN("remap") {
+      cuda::remap(input, output, map);
 
-    CHECK(output.total() == 0);
+      THEN("output must be empty") {
+        CHECK(output.total() == 0);
+      }
+    }
   }
 
-  SECTION("map with values outside of input matrix") {
+  GIVEN("not empty matrix size and random data for matrix") {
     int height = GENERATE(3, 100, 512, 1024, 1153, 2048, 2049);
     int width  = GENERATE(3, 100, 512, 1024, 1352, 2048, 2051);
     int total  = width * height;
@@ -28,38 +32,6 @@ TEST_CASE("remap", "[remap]") {
     char border_value = GENERATE(0, 1, 16, 117, -52, -34);
 
 
-    cuda::gpu_mat<char> input(height, width);
-    cuda::gpu_mat<char> output(0, 0);
-    cuda::gpu_mat<int2> map(height, width, {-1, -1});
-
-
-    // XXX in this case all values in output will be equal to border_value
-    remap(input, output, map, 0, 0, border_value);
-
-
-    std::vector<decltype(output)::value_type> output_v(total);
-    output.download(output_v.data());
-
-    auto found = std::find_if(output_v.begin(),
-                              output_v.end(),
-                              [border_value](char val) {
-                                if (val != border_value) {
-                                  return true;
-                                }
-                                return false;
-                              });
-
-    CHECK(found == output_v.end());
-  }
-
-  SECTION("process matrix with same order, so result matix must be same") {
-    int height = GENERATE(3, 100, 512, 1024, 1153, 2048, 2049);
-    int width  = GENERATE(3, 100, 512, 1024, 1352, 2048, 2051);
-    int total  = width * height;
-
-    char border_value = 0;
-
-
     std::random_device                  rd;
     std::mt19937                        gen{rd()};
     std::uniform_int_distribution<char> dist{-128, 127};
@@ -70,198 +42,192 @@ TEST_CASE("remap", "[remap]") {
       val = dist(gen);
     }
 
-    std::vector<int2> map_data(total);
-    for (int row = 0; row < height; ++row) {
-      for (int col = 0; col < width; ++col) {
-        int offset       = row * width + col;
-        map_data[offset] = make_int2(col, row);
+
+    AND_GIVEN("sunc pipeline matricies") {
+      cuda::gpu_mat<char> input(height, width, input_data.data());
+      cuda::gpu_mat<char> output(0, 0);
+
+      AND_GIVEN("map with values outside of input matrix") {
+        cuda::gpu_mat<int2> map(height, width, {-1, -1});
+
+
+        WHEN("remap") {
+          // XXX in this case all values in output will be equal to border_value
+          remap(input, output, map, 0, 0, border_value);
+
+          THEN("entire output must be equal to border value") {
+            std::vector<decltype(output)::value_type> output_v(total);
+            output.download(output_v.data());
+
+            auto found = std::find_if(output_v.begin(),
+                                      output_v.end(),
+                                      [border_value](char val) {
+                                        if (val != border_value) {
+                                          return true;
+                                        }
+                                        return false;
+                                      });
+
+            CHECK(found == output_v.end());
+          }
+        }
       }
-    }
+
+      AND_GIVEN("map with same order") {
+        std::vector<int2> map_data(total);
+        for (int row = 0; row < height; ++row) {
+          for (int col = 0; col < width; ++col) {
+            int offset       = row * width + col;
+            map_data[offset] = make_int2(col, row);
+          }
+        }
+
+        cuda::gpu_mat<int2> map(height, width, map_data.data());
 
 
-    cuda::gpu_mat<char> input(height, width, input_data.data());
-    cuda::gpu_mat<char> output(0, 0);
-    cuda::gpu_mat<int2> map(height, width, map_data.data());
+        WHEN("remap") {
+          remap(input, output, map, 0, 0, border_value);
 
 
-    remap(input, output, map, 0, 0, border_value);
+          THEN("input must be equal to output") {
+            std::vector<char> output_data(total);
+            output.download(output_data.data());
 
 
-    std::vector<char> output_data(total);
-    output.download(output_data.data());
-
-
-    CHECK(input_data == output_data);
-  }
-
-  SECTION("process matrix in revert order") {
-    int height = GENERATE(3, 100, 512, 1024, 1153, 2048, 2049);
-    int width  = GENERATE(3, 100, 512, 1024, 1352, 2048, 2051);
-    int total  = width * height;
-
-    char border_value = 0;
-
-
-    std::random_device                  rd;
-    std::mt19937                        gen{rd()};
-    std::uniform_int_distribution<char> dist{-128, 127};
-
-
-    std::vector<char> input_data(total);
-    for (char &val : input_data) {
-      val = dist(gen);
-    }
-
-    std::vector<int2> map_data(total);
-    for (int row = 0; row < height; ++row) {
-      for (int col = 0; col < width; ++col) {
-        int offset       = row * width + col;
-        map_data[offset] = make_int2(width - col - 1, height - row - 1);
+            CHECK(input_data == output_data);
+          }
+        }
       }
-    }
+
+      AND_GIVEN("map with revert order") {
+        std::vector<int2> map_data(total);
+        for (int row = 0; row < height; ++row) {
+          for (int col = 0; col < width; ++col) {
+            int offset       = row * width + col;
+            map_data[offset] = make_int2(width - col - 1, height - row - 1);
+          }
+        }
+
+        cuda::gpu_mat<int2> map(height, width, map_data.data());
 
 
-    cuda::gpu_mat<char> input(height, width, input_data.data());
-    cuda::gpu_mat<char> output(0, 0);
-    cuda::gpu_mat<int2> map(height, width, map_data.data());
+        WHEN("remap") {
+          remap(input, output, map, 0, 0, border_value);
 
 
-    remap(input, output, map, 0, 0, border_value);
+          THEN("check revert order of output") {
+            std::vector<char> output_data(total);
+            output.download(output_data.data());
 
 
-    std::vector<char> output_data(total);
-    output.download(output_data.data());
+            for (int row = 0; row < height; ++row) {
+              for (int col = 0; col < width; ++col) {
+                int offset = row * width + col;
+                int mapped_offset =
+                    (height - row - 1) * width + (width - col - 1);
 
-
-    for (int row = 0; row < height; ++row) {
-      for (int col = 0; col < width; ++col) {
-        int offset        = row * width + col;
-        int mapped_offset = (height - row - 1) * width + (width - col - 1);
-
-        REQUIRE(input_data[offset] == output_data[mapped_offset]);
+                REQUIRE(input_data[offset] == output_data[mapped_offset]);
+              }
+            }
+          }
+        }
       }
-    }
-  }
 
-  SECTION("process matrix in random order") {
-    int height = GENERATE(3, 100, 512, 1024, 1153, 2048, 2049);
-    int width  = GENERATE(3, 100, 512, 1024, 1352, 2048, 2051);
-    int total  = width * height;
+      AND_GIVEN("random order map") {
+        std::uniform_int_distribution<int> map_dist{
+            -10,
+            height > width ? height + 10 : width + 10};
 
-    char border_value = GENERATE(2, 0, -3, 7, 64, -32);
+        std::vector<int2> map_data(total);
+        for (int row = 0; row < height; ++row) {
+          for (int col = 0; col < width; ++col) {
+            int offset       = row * width + col;
+            map_data[offset] = make_int2(map_dist(gen), map_dist(gen));
+          }
+        }
 
-
-    std::random_device                  rd;
-    std::mt19937                        gen{rd()};
-    std::uniform_int_distribution<char> val_dist{-128, 127};
-    std::uniform_int_distribution<int>  map_dist{-10,
-                                                height > width ? height + 10
-                                                               : width + 10};
+        cuda::gpu_mat<int2> map(height, width, map_data.data());
 
 
-    std::vector<char> input_data(total);
-    for (char &val : input_data) {
-      val = val_dist(gen);
-    }
-
-    std::vector<int2> map_data(total);
-    for (int row = 0; row < height; ++row) {
-      for (int col = 0; col < width; ++col) {
-        int offset       = row * width + col;
-        map_data[offset] = make_int2(map_dist(gen), map_dist(gen));
-      }
-    }
+        WHEN("remap") {
+          remap(input, output, map, 0, 0, border_value);
 
 
-    cuda::gpu_mat<char> input(height, width, input_data.data());
-    cuda::gpu_mat<char> output(0, 0);
-    cuda::gpu_mat<int2> map(height, width, map_data.data());
+          THEN("check output") {
+            std::vector<char> output_data(total);
+            output.download(output_data.data());
 
 
-    remap(input, output, map, 0, 0, border_value);
+            // check
+            for (int row = 0; row < height; ++row) {
+              for (int col = 0; col < width; ++col) {
+                int  offset     = row * width + col;
+                int  x          = map_data[offset].x;
+                int  y          = map_data[offset].y;
+                char mapped_val = output_data[offset];
 
-
-    std::vector<char> output_data(total);
-    output.download(output_data.data());
-
-
-    // check
-    for (int row = 0; row < height; ++row) {
-      for (int col = 0; col < width; ++col) {
-        int  offset     = row * width + col;
-        int  x          = map_data[offset].x;
-        int  y          = map_data[offset].y;
-        char mapped_val = output_data[offset];
-
-        if (x >= 0 && x < width && y >= 0 && y < height) {
-          int orig_val = input_data[y * width + x];
-          REQUIRE(mapped_val == orig_val);
-        } else {
-          REQUIRE(mapped_val == border_value);
+                if (x >= 0 && x < width && y >= 0 && y < height) {
+                  int orig_val = input_data[y * width + x];
+                  REQUIRE(mapped_val == orig_val);
+                } else {
+                  REQUIRE(mapped_val == border_value);
+                }
+              }
+            }
+          }
         }
       }
     }
-  }
 
-  SECTION("async process matrix in random order with stream") {
-    int height = GENERATE(3, 100, 512, 1024, 1153, 2048, 2049);
-    int width  = GENERATE(3, 100, 512, 1024, 1352, 2048, 2051);
-    int total  = width * height;
-
-    char border_value = GENERATE(2, 0, -3, 7, 64, -32);
+    AND_GIVEN("stream pipeline") {
+      cuda::stream        stream{1};
+      cuda::gpu_mat<char> input(height, width, input_data.data(), stream);
 
 
-    std::random_device                  rd;
-    std::mt19937                        gen{rd()};
-    std::uniform_int_distribution<char> val_dist{-128, 127};
-    std::uniform_int_distribution<int>  map_dist{-10,
-                                                height > width ? height + 10
-                                                               : width + 10};
+      AND_GIVEN("map with random values") {
+        std::uniform_int_distribution<int> map_dist{
+            -10,
+            height > width ? height + 10 : width + 10};
+
+        std::vector<int2> map_data(total);
+        for (int row = 0; row < height; ++row) {
+          for (int col = 0; col < width; ++col) {
+            int offset       = row * width + col;
+            map_data[offset] = make_int2(map_dist(gen), map_dist(gen));
+          }
+        }
+
+        cuda::gpu_mat<int2> map(height, width, map_data.data(), stream);
 
 
-    std::vector<char> input_data(total);
-    for (char &val : input_data) {
-      val = val_dist(gen);
-    }
+        WHEN("remap") {
+          cuda::gpu_mat<char> output(0, 0, stream);
+          remap(input, output, map, 0, 0, border_value, stream);
 
-    std::vector<int2> map_data(total);
-    for (int row = 0; row < height; ++row) {
-      for (int col = 0; col < width; ++col) {
-        int offset       = row * width + col;
-        map_data[offset] = make_int2(map_dist(gen), map_dist(gen));
-      }
-    }
+          THEN("check result") {
+            // XXX async downloading works in other way, so be carefull!
+            output.download(stream);
+            stream.synchronize();
+            const char *output_data = output.host_ptr();
 
 
-    cuda::stream stream{1};
+            // check
+            for (int row = 0; row < height; ++row) {
+              for (int col = 0; col < width; ++col) {
+                int  offset     = row * width + col;
+                int  x          = map_data[offset].x;
+                int  y          = map_data[offset].y;
+                char mapped_val = output_data[offset];
 
-    cuda::gpu_mat<char> input(height, width, input_data.data(), stream);
-    cuda::gpu_mat<char> output(0, 0, stream);
-    cuda::gpu_mat<int2> map(height, width, map_data.data(), stream);
-
-
-    remap(input, output, map, 0, 0, border_value, stream);
-
-
-    // XXX async downloading works in other way, so be carefull!
-    output.download(stream);
-    stream.synchronize();
-    const char *output_data = output.host_ptr();
-
-
-    // check
-    for (int row = 0; row < height; ++row) {
-      for (int col = 0; col < width; ++col) {
-        int  offset     = row * width + col;
-        int  x          = map_data[offset].x;
-        int  y          = map_data[offset].y;
-        char mapped_val = output_data[offset];
-
-        if (x >= 0 && x < width && y >= 0 && y < height) {
-          int orig_val = input_data[y * width + x];
-          REQUIRE(mapped_val == orig_val);
-        } else {
-          REQUIRE(mapped_val == border_value);
+                if (x >= 0 && x < width && y >= 0 && y < height) {
+                  int orig_val = input_data[y * width + x];
+                  REQUIRE(mapped_val == orig_val);
+                } else {
+                  REQUIRE(mapped_val == border_value);
+                }
+              }
+            }
+          }
         }
       }
     }
--- test/warp.cu
+++ test/warp.cu
@@ -5,23 +5,30 @@
 
 //
 
+
 #include "cuda/warp.hpp"
 #include <iostream>
 #include <random>
 
-TEST_CASE("warp", "[warp]") {
-  SECTION("try warp empty matrix") {
+
+SCENARIO("warp", "[warp]") {
+  GIVEN("empty matrix") {
     cuda::gpu_mat<int> input{0, 0, 0};
     cuda::gpu_mat<int> output{0, 0};
 
     cuda::affine_matrix      mat_a{{1, 0, 0, 0, 1, 0}};
     cuda::perspective_matrix mat_p{{1, 0, 0, 0, 1, 0, 0, 0, 1}};
 
-    cuda::warpAffine(input, output, mat_a);
-    cuda::warpPerspective(input, output, mat_p);
+    WHEN("warp") {
+      cuda::warpAffine(input, output, mat_a);
+      cuda::warpPerspective(input, output, mat_p);
+
+      THEN("haven't error about zero division") {
+      }
+    }
   }
 
-  SECTION("warp with identity matrix and different sizes") {
+  GIVEN("different matrix input/output sizes and some random data") {
     int height        = GENERATE(3, 100, 512, 1024, 1153, 2048, 2049);
     int width         = GENERATE(3, 100, 512, 1024, 1352, 2048, 2051);
     int height_output = GENERATE(3, 100, 512, 1024, 1153, 2048, 2049);
@@ -30,6 +37,7 @@ TEST_CASE("warp", "[warp]") {
 
     char border_value = 5;
 
+
     std::random_device                  rd;
     std::mt19937                        gen{rd()};
     std::uniform_int_distribution<char> dist{-128, 127};
@@ -40,154 +48,143 @@ TEST_CASE("warp", "[warp]") {
     }
 
 
-    cuda::gpu_mat<char> input(height, width, input_data.data());
-    cuda::gpu_mat<char> output_a(height_output, width_output);
-    cuda::gpu_mat<char> output_p(height_output, width_output);
+    AND_GIVEN("sync pipeline") {
+      cuda::gpu_mat<char> input(height, width, input_data.data());
 
-    cuda::affine_matrix      mat_a{{1, 0, 0, 0, 1, 0}};
-    cuda::perspective_matrix mat_p{{1, 0, 0, 0, 1, 0, 0, 0, 1}};
-
-
-    cuda::warpAffine(input, output_a, mat_a, 0, 0, border_value);
-    cuda::warpPerspective(input, output_p, mat_p, 0, 0, border_value);
+      cuda::gpu_mat<char> output_a(height_output, width_output);
+      cuda::gpu_mat<char> output_p(height_output, width_output);
 
 
-    std::vector<char> output_data_a(output_a.total());
-    output_a.download(output_data_a.data());
-
-    std::vector<char> output_data_p(output_p.total());
-    output_p.download(output_data_p.data());
+      AND_GIVEN("identity matrix") {
+        cuda::affine_matrix      mat_a{{1, 0, 0, 0, 1, 0}};
+        cuda::perspective_matrix mat_p{{1, 0, 0, 0, 1, 0, 0, 0, 1}};
 
 
-    REQUIRE(output_data_p == output_data_a);
+        WHEN("warp") {
+          cuda::warpAffine(input, output_a, mat_a, 0, 0, border_value);
+          cuda::warpPerspective(input, output_p, mat_p, 0, 0, border_value);
 
-    for (size_t row = 0; row < height_output; ++row) {
-      for (size_t col = 0; col < width_output; ++col) {
-        size_t offset_out = row * width_output + col;
-        size_t offset_in  = row * width + col;
+          THEN("output must be same as input") {
+            std::vector<char> output_data_a(output_a.total());
+            output_a.download(output_data_a.data());
 
-        if (row < height && col < width) {
-          REQUIRE(output_data_a[offset_out] == input_data[offset_in]);
-        } else {
-          REQUIRE(output_data_a[offset_out] == border_value);
+            std::vector<char> output_data_p(output_p.total());
+            output_p.download(output_data_p.data());
+
+
+            REQUIRE(output_data_p == output_data_a);
+
+            for (size_t row = 0; row < height_output; ++row) {
+              for (size_t col = 0; col < width_output; ++col) {
+                size_t offset_out = row * width_output + col;
+                size_t offset_in  = row * width + col;
+
+                if (row < height && col < width) {
+                  REQUIRE(output_data_a[offset_out] == input_data[offset_in]);
+                } else {
+                  REQUIRE(output_data_a[offset_out] == border_value);
+                }
+              }
+            }
+          }
+        }
+      }
+
+      AND_GIVEN("move matrix") {
+        int min_side = height < width ? height : width;
+        std::uniform_int_distribution<int> move_dist{-min_side, min_side};
+        float                              move_x = move_dist(gen);
+        float                              move_y = move_dist(gen);
+
+
+        cuda::affine_matrix      mat_a{{1, 0, move_x, 0, 1, move_y}};
+        cuda::perspective_matrix mat_p{{1, 0, move_x, 0, 1, move_y, 0, 0, 1}};
+
+
+        WHEN("warp") {
+          cuda::warpAffine(input, output_a, mat_a, 0, 0, border_value);
+          cuda::warpPerspective(input, output_p, mat_p, 0, 0, border_value);
+
+
+          THEN("check that output matrix was moved") {
+            std::vector<char> output_data_a(output_a.total());
+            output_a.download(output_data_a.data());
+
+            std::vector<char> output_data_p(output_p.total());
+            output_p.download(output_data_p.data());
+
+
+            REQUIRE(output_data_p == output_data_a);
+
+
+            for (size_t row = 0; row < height_output; ++row) {
+              for (size_t col = 0; col < width_output; ++col) {
+                size_t offset_out = row * width_output + col;
+
+                size_t input_row = row - move_y;
+                size_t input_col = col - move_x;
+                if (input_row < height && input_col < width) {
+                  size_t offset_in = input_row * width + input_col;
+                  REQUIRE(output_data_a[offset_out] == input_data[offset_in]);
+                } else {
+                  REQUIRE(output_data_a[offset_out] == border_value);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+
+    GIVEN("async pipeline") {
+      cuda::stream        stream{1};
+      cuda::gpu_mat<char> input(height, width, input_data.data(), stream);
+      cuda::gpu_mat<char> output_async(height, width, stream);
+      cuda::gpu_mat<char> output_sync(height, width);
+
+
+      AND_GIVEN("affine transformation matrix") {
+        std::uniform_int_distribution<int> t_mat_val_dist{-10, 10};
+        cuda::affine_matrix                mat_a{{(float)t_mat_val_dist(gen),
+                                   (float)t_mat_val_dist(gen),
+                                   (float)t_mat_val_dist(gen),
+                                   (float)t_mat_val_dist(gen),
+                                   (float)t_mat_val_dist(gen),
+                                   (float)t_mat_val_dist(gen)}};
+
+
+        WHEN("affine warp") {
+          cuda::warpAffine(input,
+                           output_async,
+                           mat_a,
+                           0,
+                           0,
+                           border_value,
+                           stream);
+          cuda::warpAffine(input, output_sync, mat_a, 0, 0, border_value);
+
+
+          THEN("check that async and sync outputs are equal") {
+            output_async.download(stream);
+            stream.synchronize();
+
+
+            std::vector<char> output_data_async(output_async.total());
+            std::memcpy(output_data_async.data(),
+                        output_async.host_ptr(),
+                        output_data_async.size());
+
+            std::vector<char> output_data_sync(output_sync.total());
+            output_sync.download(output_data_sync.data());
+
+
+            REQUIRE(output_data_sync == output_data_async);
+          }
         }
       }
     }
   }
-
-  SECTION("warp with move matrix") {
-    int height = GENERATE(3, 100, 512, 1024, 1153, 2048, 2049);
-    int width  = GENERATE(3, 100, 512, 1024, 1352, 2048, 2051);
-    int total  = width * height;
-
-    char border_value = 7;
-
-    std::random_device                  rd;
-    std::mt19937                        gen{rd()};
-    std::uniform_int_distribution<char> dist{-128, 127};
-
-    std::vector<char> input_data(total);
-    for (char &val : input_data) {
-      val = dist(gen);
-    }
-
-
-    int min_side = height < width ? height : width;
-    std::uniform_int_distribution<int> move_dist{-min_side, min_side};
-    float                              move_x = move_dist(gen);
-    float                              move_y = move_dist(gen);
-
-
-    cuda::gpu_mat<char> input(height, width, input_data.data());
-    cuda::gpu_mat<char> output_a(height, width);
-    cuda::gpu_mat<char> output_p(height, width);
-
-    cuda::affine_matrix      mat_a{{1, 0, move_x, 0, 1, move_y}};
-    cuda::perspective_matrix mat_p{{1, 0, move_x, 0, 1, move_y, 0, 0, 1}};
-
-
-    cuda::warpAffine(input, output_a, mat_a, 0, 0, border_value);
-    cuda::warpPerspective(input, output_p, mat_p, 0, 0, border_value);
-
-
-    std::vector<char> output_data_a(output_a.total());
-    output_a.download(output_data_a.data());
-
-    std::vector<char> output_data_p(output_p.total());
-    output_p.download(output_data_p.data());
-
-
-    REQUIRE(output_data_p == output_data_a);
-
-    for (size_t row = 0; row < height; ++row) {
-      for (size_t col = 0; col < width; ++col) {
-        size_t offset_out = row * width + col;
-
-        size_t input_row = row - move_y;
-        size_t input_col = col - move_x;
-        if (input_row >= height || input_col >= width) {
-          REQUIRE(output_data_a[offset_out] == border_value);
-        } else {
-          size_t offset_in = input_row * width + input_col;
-
-          REQUIRE(output_data_a[offset_out] == input_data[offset_in]);
-        }
-      }
-    }
-  }
-
-  SECTION("test async warping") {
-    int height = GENERATE(3, 100, 512, 1024, 1153, 2048, 2049);
-    int width  = GENERATE(3, 100, 512, 1024, 1352, 2048, 2051);
-    int total  = width * height;
-
-    char border_value = 8;
-
-    std::random_device                  rd;
-    std::mt19937                        gen{rd()};
-    std::uniform_int_distribution<char> dist{-128, 127};
-
-    std::vector<char> input_data(total);
-    for (char &val : input_data) {
-      val = dist(gen);
-    }
-
-
-    cuda::stream        stream{1};
-    cuda::gpu_mat<char> input(height, width, input_data.data(), stream);
-    cuda::gpu_mat<char> output_async(height, width, stream);
-    cuda::gpu_mat<char> output_sync(height, width);
-
-
-    std::uniform_int_distribution<int> t_mat_val_dist{-10, 10};
-    cuda::affine_matrix                mat_a{{(float)t_mat_val_dist(gen),
-                               (float)t_mat_val_dist(gen),
-                               (float)t_mat_val_dist(gen),
-                               (float)t_mat_val_dist(gen),
-                               (float)t_mat_val_dist(gen),
-                               (float)t_mat_val_dist(gen)}};
-
-
-    cuda::warpAffine(input, output_async, mat_a, 0, 0, border_value, stream);
-    cuda::warpAffine(input, output_sync, mat_a, 0, 0, border_value);
-
-
-    output_async.download(stream);
-    stream.synchronize();
-
-
-    std::vector<char> output_data_async(output_async.total());
-    std::memcpy(output_data_async.data(),
-                output_async.host_ptr(),
-                output_data_async.size());
-
-    std::vector<char> output_data_sync(output_sync.total());
-    output_sync.download(output_data_sync.data());
-
-
-    REQUIRE(output_data_sync == output_data_async);
-  }
-
-  // TODO add test for warping with rotaion matrix
-  // TODO add test for complete perspective warping
 }
+
+// TODO add test for warping with rotaion matrix
+// TODO add test for complete perspective warping
